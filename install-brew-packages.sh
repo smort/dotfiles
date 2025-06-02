bed36f1406a4fcd5949f1b2ae3ad38d5727ccd74
smort <smort@users.noreply.github.com>
2025-06-01 22:20:32 -0600

    add brew bin dir to PATH

--- install-brew-packages.sh
+++ install-brew-packages.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-export PATH=/home/linuxbrew/.linuxbrew:$PATH
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 
 PACKAGE_FILE="brew-packages.conf"
 
