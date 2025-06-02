d9e723eae00514f9d0ebfbc363892a46acc2a34a
smort <smort@users.noreply.github.com>
2025-06-01 22:14:47 -0600

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,8 @@ if test -d ~/.local/bin
     set -U fish_user_paths ~/.local/bin $fish_user_paths
 end
 
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+
 # Set up fzf key bindings
 fzf --fish | source
 
@@ -10,6 +12,3 @@ fzf --fish | source
 source ~/.config/fish/conf.d/fnm.fish
 
 starship init fish | source
-
-fish_add_path /home/linuxbrew/.linuxbrew/bin
-
